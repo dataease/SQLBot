794b19e943540487f4452393d61315801d185312
wangjiahao <1522128093@qq.com>
2026-04-27 10:16:23 +0800

    fix(dashboard): Fix the issue where the predicted data cannot be displayed on the data dashboard

--- frontend/src/views/chat/chat-block/ChartBlock.vue
+++ frontend/src/views/chat/chat-block/ChartBlock.vue
@@ -219,6 +219,8 @@ const showLabel = ref(false)
 function addToDashboard() {
   const recordeInfo = {
     id: '1-1',
+    sourceRecordId: props.message?.record.id,
+    sourceChatId: props.message?.record.chat_id,
     data: {
       data: data.value,
     },
